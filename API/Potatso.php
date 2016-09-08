2759613be6108d948832649236592dd0fbf71f08
BurpSuite <BurpSuite@iCloud.com>
2016-09-08 12:23:08 +0800

    UPdate
    UPdate

--- API/Potatso.php
+++ API/Potatso.php
@@ -1,37 +1,30 @@
 <?php
 //------------Start-------------//
-header("cache-control:no-cache,must-revalidate");//No-Cache
-header("Content-Type:text/html;charset=UTF-8");//UTF-8
+header("cache-control:no-cache,must-revalidate");
+header("Content-Type:text/html;charset=UTF-8");
+header("Accept-Ranges: bytes");
+header('Content-Disposition: attachment; filename='.'Potatso.Conf');
 //-------------通用-------------//
 $NAME = "UPlus";            //名称
 //-------------文件-------------//
 $DefaultFile = "http://7xpphx.com1.z0.glb.clouddn.com/Proxy/File/Default.txt";
-$DefaultFile  = $DefaultFile . '?Cache='.time();
+$DefaultFile  = $DefaultFile . '?Sign='.sha1(mt_rand()).'&TimeStamp='.time();
 $Default = fopen($DefaultFile,"r");
 $ProxyFile = "http://7xpphx.com1.z0.glb.clouddn.com/Proxy/File/Proxy.txt";
-$ProxyFile  = $ProxyFile . '?Cache='.time();
+$ProxyFile  = $ProxyFile . '?Sign='.sha1(mt_rand()).'&TimeStamp='.time();
 $Proxy = fopen($ProxyFile,"r");
 $DIRECTFile = "http://7xpphx.com1.z0.glb.clouddn.com/Proxy/File/DIRECT.txt";
-$DIRECTFile  = $DIRECTFile . '?Cache='.time();
+$DIRECTFile  = $DIRECTFile . '?Sign='.sha1(mt_rand()).'&TimeStamp='.time();
 $DIRECT = fopen($DIRECTFile,"r");
 $REJECTFile = "http://7xpphx.com1.z0.glb.clouddn.com/Proxy/File/REJECT.txt";
-$REJECTFile  = $REJECTFile . '?Cache='.time();
+$REJECTFile  = $REJECTFile . '?Sign='.sha1(mt_rand()).'&TimeStamp='.time();
 $REJECT = fopen($REJECTFile,"r");
-//$PathFile = "http://7xpphx.com1.z0.glb.clouddn.com/Proxy/File/Path.txt";
-//$PathFile  = $PathFile . '?Cache='.time();
-//$Path = fopen($PathFile,"r");
 $KEYWORDFile = "http://7xpphx.com1.z0.glb.clouddn.com/Proxy/File/KEYWORD.txt";
-$KEYWORDFile  = $KEYWORDFile . '?Cache='.time();
+$KEYWORDFile  = $KEYWORDFile . '?Sign='.sha1(mt_rand()).'&TimeStamp='.time();
 $KEYWORD = fopen($KEYWORDFile,"r");
 $IPCIDRFile = "http://7xpphx.com1.z0.glb.clouddn.com/Proxy/File/IPCIDR.txt";
-$IPCIDRFile  = $IPCIDRFile . '?Cache='.time();
+$IPCIDRFile  = $IPCIDRFile . '?Sign='.sha1(mt_rand()).'&TimeStamp='.time();
 $IPCIDR = fopen($IPCIDRFile,"r");
-//-------------下载-------------//
-$File = "Potatso.Conf";//下载文件名称
-header("cache-control:no-cache,must-revalidate");//No-Cache
-header('Content-type: application/octet-stream; charset=utf8');//下载动作
-header("Accept-Ranges: bytes");
-header('Content-Disposition: attachment; filename='.$File);//名称
 //--------------配置------------//
 echo"ruleSets:\r\n";
 echo"# \r\n";
@@ -40,9 +33,9 @@ echo"# Last Modified: " . date("Y/m/d") . "\r\n";
 echo"#\r\n";
 echo"- name: $NAME\r\n";
 echo"  rules: ";
-//--------------输出------------//
+//--------------模块------------//
 //Default
-if($Default){//判断打开错误
+if($Default){
 echo"\r\n# Default\r\n";
 while(!feof($Default))
 {
@@ -53,10 +46,10 @@ echo trim(fgets($Default)).",DIRECT"."\r\n";
 fclose($Default);
 }
 }else {
-  echo "\r\n# Default Module下载失败!\r\n";//
+  echo "\r\n# Default Module下载失败!\r\n";
 }
 //PROXY
-if($Proxy){//判断打开错误
+if($Proxy){
 echo"# PROXY\r\n";
 while(!feof($Proxy))
 {
@@ -67,10 +60,10 @@ echo trim(fgets($Proxy)).",Proxy"."\r\n";
 fclose($Proxy);
 }
 }else {
-  echo "\r\n# Proxy Module下载失败!\r\n";//
+  echo "\r\n# Proxy Module下载失败!\r\n";
 }
 //DIRECT
-if($DIRECT){//判断打开错误
+if($DIRECT){
 echo"# DIRECT\r\n";
 while(!feof($DIRECT))
 {
@@ -81,10 +74,10 @@ echo trim(fgets($DIRECT)).",DIRECT"."\r\n";
 fclose($DIRECT);
 }
 }else {
-  echo "\r\n# DIRECT Module下载失败!\r\n";//
+  echo "\r\n# DIRECT Module下载失败!\r\n";
 }
 //REJECT
-if($REJECT){//判断打开错误
+if($REJECT){
 echo"\r\n# REJECT\r\n";
 while(!feof($REJECT))
 {
@@ -95,24 +88,10 @@ echo trim(fgets($REJECT)).",REJECT"."\r\n";
 fclose($REJECT);
 }
 }else {
-  echo "\r\n# REJECT Module下载失败!\r\n";//
+  echo "\r\n# REJECT Module下载失败!\r\n";
 }
-//URL-MATCH
-//if($Path){//判断打开错误
-//echo"# URL-MATCH\r\n";
-//while(!feof($Path))
-//{
-//echo "  - URL-MATCH,";
-//echo fgets($Path)."";
-//}
-//{
-//fclose($Path);
-//}
-//}else {//
-//  echo "下载失败!";//
-//}
 //DOMAIN-MATCH
-if($KEYWORD){//判断打开错误
+if($KEYWORD){
 echo"\r\n# DOMAIN-MATCH\r\n";
 while(!feof($KEYWORD))
 {
@@ -123,10 +102,10 @@ echo fgets($KEYWORD)."";
 fclose($KEYWORD);
 }
 }else {
-  echo "\r\n# KEYWORD Module下载失败!\r\n";//
+  echo "\r\n# KEYWORD Module下载失败!\r\n";
 }
 //IPCIDR
-if($IPCIDR){//判断打开错误
+if($IPCIDR){
 echo"\r\n# IP-CIDR\r\n";
 while(!feof($IPCIDR))
 {
@@ -137,7 +116,7 @@ echo fgets($IPCIDR)."";
 fclose($IPCIDR);
 }
 }else {
-  echo "\r\n# IPCIDR Module下载失败!\r\n";//
+  echo "\r\n# IPCIDR Module下载失败!\r\n";
 }
 //Other
 echo"\r\n#Other\r\n";
