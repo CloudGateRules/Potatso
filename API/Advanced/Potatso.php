154d1d991ca884bb46871e147834016a039606e8
BurpSuite <BurpSuite@iCloud.com>
2016-12-03 14:36:25 +0800

    Fix Bug

--- API/Advanced/Potatso.php
+++ API/Advanced/Potatso.php
@@ -1,12 +1,17 @@
 <?php
 
-# 关闭所有 Notice | Warning 级别的错误
-error_reporting(E_ALL^E_NOTICE^E_WARNING);
-
-# 页面禁止缓存 | UTF-8编码 | 触发下载
-header("cache-control:no-cache,must-revalidate");
-header("Content-Type:text/html;charset=UTF-8");
+# 触发下载
 header('Content-Disposition: attachment; filename='.'Potatso.Conf');
+
+# 设置开启哪些模块 | 必须放置在Controller控制器前面
+$DefaultModule  = "true";
+$AdvancedModule = "true";
+$BasicModule    = "true";
+$DIRECTModule   = "true";
+$REJECTModule   = "true";
+$KEYWORDModule  = "true";
+$IPCIDRModule   = "true";
+$OtherModule    = "true";
 
 # 引用Controller控制器模块
 require '../Controller/Controller.php';
